975f3f1a19b86c86cd6284c39eead0b3e6546bc1
Tomz Phoz <dsign-life@hotmail.com>
2020-12-29 20:48:47 +0100

    re commit

---
+++ reactfox/Models/Currencies.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reactfox.Models
+{
+    public class Currencies
+    {
+        public USD USD { get; set; }
+        public AUD AUD { get; set; }
+        public BRL BRL { get; set; }
+        public CAD CAD { get; set; }
+        public CHF CHF { get; set; }
+        public CLP CLP { get; set; }
+        public CNY CNY { get; set; }
+        public DKK DKK { get; set; }
+        public EUR EUR { get; set; }
+        public GDP GDP { get; set; }
+        public HKD HKD { get; set; }
+        public INR INR { get; set; }
+        public ISK ISK { get; set; }
+        public JPY JPY { get; set; }
+        public KRW KRW { get; set; }
+        public NZD NZD { get; set; }
+        public PLN PLN { get; set; }
+        public RUB RUB { get; set; }
+        public SEK SEK { get; set; }
+        public SGD SGD { get; set; }
+        public THB THB { get; set; }
+        public TRY TRY { get; set; }
+        public TWD TWD { get; set; }
+
+
+    }
+    public class USD : BTCExchangeRates
+    {
+
+    }
+    public class AUD : BTCExchangeRates
+    {
+
+    }
+    public class BRL : BTCExchangeRates
+    {
+
+    }
+    public class CAD : BTCExchangeRates
+    {
+
+    }
+    public class CHF : BTCExchangeRates
+    {
+
+    }
+    public class CLP : BTCExchangeRates
+    {
+
+    }
+    public class CNY : BTCExchangeRates
+    {
+
+    }
+    public class DKK : BTCExchangeRates
+    {
+
+    }
+    public class EUR : BTCExchangeRates
+    {
+
+    }
+    public class GDP : BTCExchangeRates
+    {
+
+    }
+    public class HKD : BTCExchangeRates
+    {
+
+    }
+    public class INR : BTCExchangeRates
+    {
+
+    }
+    public class ISK : BTCExchangeRates
+    {
+
+    }
+    public class JPY : BTCExchangeRates
+    {
+
+    }
+
+    public class KRW : BTCExchangeRates
+    {
+
+    }
+    public class NZD : BTCExchangeRates
+    {
+
+    }
+    public class PLN : BTCExchangeRates
+    {
+
+    }
+    public class RUB : BTCExchangeRates
+    {
+
+    }
+    public class SEK : BTCExchangeRates
+    {
+
+    }
+    public class SGD : BTCExchangeRates
+    {
+
+    }
+    public class THB : BTCExchangeRates
+    {
+
+    }
+
+    public class TRY : BTCExchangeRates
+    {
+
+    }
+    public class TWD : BTCExchangeRates
+    {
+
+    }
+}
---
+++ reactfox/Services/ExchangeRatesService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using reactfox.Interfaces;
+using reactfox.Models;
+
+namespace reactfox.Interface
+{
+    public class ExchangeRatesService : IExchangeRatesService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ExchangeRatesService(HttpClient httpclient)
+        {
+            _httpClient = httpclient;
+        }
+        public async Task<Currencies> GetCurrencyCodes()
+        {
+            return await Task.Run(() => GetLastestCurrencyCodes());
+        }
+
+        public async Task<Currencies> GetLastestCurrencyCodes()
+        {
+            var apiData = await JsonSerializer.DeserializeAsync<Currencies>
+                (await _httpClient.GetStreamAsync($"https://blockchain.info/ticker"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+            return apiData;
+        }
+
+    }
+}
